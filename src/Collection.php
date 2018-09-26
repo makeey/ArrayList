21f72d7301e333b42d29e7cf00d5e1ac206cbd64
Anton Makieiev <a.makieiev@owox.com>
2018-09-26 10:53:09 +0300

    Added Collection Interface

---
+++ src/Collection.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ArrayList;
+
+interface Collection extends \Iterator
+{
+    public function add(...$args): bool;
+
+    public function addAll(Collection $collection): bool;
+
+    public function clear(): void;
+
+    public function contains($object): bool;
+
+    public function containsAll(Collection $collection): bool;
+
+    public function isEmpty(): bool;
+
+    public function remove(...$args);
+
+    public function removeAll(Collection $collection): bool;
+
+    public function toArray(): array;
+
+    public function size(): int;
+
+}
